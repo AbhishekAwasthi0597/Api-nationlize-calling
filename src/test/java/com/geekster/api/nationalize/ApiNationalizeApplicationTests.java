06fd41154fe36678e84b9e5d54d153c1dc5b72fc
Abhishek <abhishekawasthi0597@gmail.com>
2023-04-12 23:58:51 +0530

    change

---
+++ src/test/java/com/geekster/api/nationalize/ApiNationalizeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekster.api.nationalize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiNationalizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
